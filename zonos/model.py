3d8c6f6d394e72eccf2a6b986b5fb2de0a549d01
sekstini <sekstinilol@gmail.com>
2025-02-11 16:43:39 +0100

    Add trailoff mechanism to avoid cutting off the end of samples

--- zonos/model.py
+++ zonos/model.py
@@ -5,7 +5,7 @@ import torch
 import torch.nn as nn
 from huggingface_hub import hf_hub_download
 from mamba_ssm.utils.generation import InferenceParams
-from tqdm import trange
+from tqdm import tqdm
 
 from zonos.autoencoder import DACAutoencoder
 from zonos.backbone import ZonosBackbone
@@ -179,11 +179,6 @@ class Zonos(nn.Module):
             ]
         )
 
-    def _disallow_cb_not_zero_eos(self, logits):
-        eos_bias = torch.zeros_like(logits)
-        eos_bias[:, 1:, self.eos_token_id] = -1e9
-        return logits + eos_bias
-
     @torch.inference_mode()
     def generate(
         self,
@@ -193,6 +188,7 @@ class Zonos(nn.Module):
         cfg_scale: float = 2.0,
         batch_size: int = 1,
         sampling_params: dict = dict(min_p=0.1),
+        progress_bar: bool = True,
     ):
         assert cfg_scale != 1, "TODO: add support for cfg_scale=1"
         prefix_audio_len = 0 if audio_prefix_codes is None else audio_prefix_codes.shape[2]
@@ -221,18 +217,40 @@ class Zonos(nn.Module):
         inference_params.seqlen_offset += prefix_length
         inference_params.lengths_per_sample[:] += prefix_length
 
-        for offset in trange(offset + 1, delayed_codes.shape[2]):
+        logit_bias = torch.zeros_like(logits)
+        logit_bias[:, 1:, self.eos_token_id] = -torch.inf  # only allow codebook 0 to predict EOS
+
+        stopping = torch.zeros(batch_size, dtype=torch.bool, device="cuda")
+        remaining_steps = torch.full((batch_size,), delayed_codes.shape[2] - offset, device="cuda")
+        progress = tqdm(total=remaining_steps.max().item(), desc="Generating", disable=not progress_bar)
+
+        while torch.max(remaining_steps) > 0:
+            offset += 1
             input_ids = delayed_codes[..., offset - 1 : offset]
             logits = self._decode_one_token(input_ids, inference_params, cfg_scale)
-            logits = self._disallow_cb_not_zero_eos(logits)
+
             next_token = sample_from_logits(logits, generated_tokens=delayed_codes[..., :offset], **sampling_params)
-            if offset > 8 and (next_token == self.eos_token_id).any():
-                break
+            eos_in_cb0 = next_token[:, 0] == self.eos_token_id
+
+            remaining_steps[eos_in_cb0[:, 0]] = torch.minimum(remaining_steps[eos_in_cb0[:, 0]], torch.tensor(9))
+            stopping |= eos_in_cb0[:, 0]
+
+            eos_codebook_idx = 9 - remaining_steps
+            eos_codebook_idx = torch.clamp(eos_codebook_idx, max=9 - 1)
+            for i in range(next_token.shape[0]):
+                if stopping[i]:
+                    idx = eos_codebook_idx[i].item()
+                    next_token[i, :idx] = self.masked_token_id
+                    next_token[i, idx] = self.eos_token_id
 
             frame = delayed_codes[..., offset : offset + 1]
             frame.masked_scatter_(frame == unknown_token, next_token)
             inference_params.seqlen_offset += 1
             inference_params.lengths_per_sample[:] += 1
+
+            remaining_steps -= 1
+
+            progress.update()
 
         out_codes = revert_delay_pattern(delayed_codes)
         out_codes.masked_fill_(out_codes >= 1024, 0)
